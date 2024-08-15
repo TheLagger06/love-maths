8076e98584bc934f287ffada09dc435372395a73
Hugo Abreu <hrabreu06@gmail.com>
2024-08-15 08:55:18 +0000

    Set up structure of a game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-console.log("Connected!")
+function rungame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function IncrementScore(){
+
+}
+
+ function incrementWrongAnswer(){
+
+ }
+
+ function displayAdditionQuestion(){
+
+ }
+
+ function displaySubtractQuestion(){
+
+ }
+
+ function displayMultiplyQuestion(){
+
+ }
+
+ 
